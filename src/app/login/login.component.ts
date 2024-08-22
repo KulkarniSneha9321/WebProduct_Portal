81861e44db0353dcb35415d4ddeb06e7e6cd9d07
Sneha Shrinivas <Sneha.Shrinivas@godigit.com>
2024-08-23 00:41:12 +0530

    Updated:Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validator,FormBuilder, Validators } from '@angular/forms';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -10,6 +10,30 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm:FormGroup;
+  constructor(private fb:FormBuilder, private authService: AuthService, private router: Router){
+    this.loginForm=this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',Validators.required]
+    });
+  }
+  ngOnInit(): void {}
+  
+    get email(){
+      return this.loginForm.get('email');
+    }
+    get password(){
+      return this.loginForm.get('password');
+    }
+    onsubmit(){
+      if(this.loginForm.valid){
+        this.authService.login(this.loginForm.value).subscribe(()=>{
+          this.router.navigate(['/dashboard']);
+        });
+      }
+    }
+    
+  
   
 }
